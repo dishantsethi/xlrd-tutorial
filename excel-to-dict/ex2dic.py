7a19d0a9bda9859fa52b8220008dde95e3396908
dishantsethi <dishantsethi14@gmail.com>
2018-10-01 13:55:40 +0530

    xlrd python package

--- excel-to-dict/ex2dic.py
+++ excel-to-dict/ex2dic.py
@@ -5,25 +5,35 @@ wb = xlrd.open_workbook(loc)
 sheet=wb.sheet_by_index(0)
 
 
-# print('Sheet name:',sheet.name) # name of sheet
-# print('number of sheets:',wb.nsheets) # number of sheets
-# print('number of rows:',sheet.nrows) # number of rows
-# print('number of cols:',sheet.ncols) # number of cols
+print('Sheet name:',sheet.name) # name of sheet
+print('number of sheets:',wb.nsheets) # number of sheets
+print('number of rows:',sheet.nrows) # number of rows
+print('number of cols:',sheet.ncols) # number of cols
 
 
-# for i in range(sheet.nrows):
-#     print(sheet.cell_value(i,0))
 
-# for j in range(sheet.ncols):
-#     print(sheet.cell_value(0,j))    
+#print a particular row
+for i in range(sheet.nrows):
+    print(sheet.cell_value(i,0))
 
-# data = [[sheet.cell_value(r,c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
-# print(data[0][0])
 
-# for r in range(sheet.nrows):
-#     for c in range(sheet.ncols):
-#         print(list(sheet.cell_value(r,c))) 
+#print a particular column
+for j in range(sheet.ncols):
+    print(sheet.cell_value(0,j))    
 
+
+#extract data of particular cell
+data = [[sheet.cell_value(r,c) for c in range(sheet.ncols)] for r in range(sheet.nrows)]
+print(data[0][0])
+
+
+#print whole excel sheet
+for r in range(sheet.nrows):
+    for c in range(sheet.ncols):
+        print(list(sheet.cell_value(r,c))) 
+
+
+#save data of all rows in a dictionary
 d = {}
 for i in range(sheet.nrows):
     for j in range(sheet.ncols):
